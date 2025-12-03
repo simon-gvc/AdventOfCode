68f8fbf4bbd3a57ca5c1aad9ec18983b318f1c3b
simon-gvc <simon@gvc.tax>
2025-12-03 22:02:03 +0100

    Cleanup

--- 2025/Day3.cpp
+++ 2025/Day3.cpp
@@ -19,7 +19,6 @@ std::string Day3::handlePart1Impl(const std::string& input) {
     while (std::getline(file, line)) {
         int left_index = 0;
         int left_value = 0;
-        int right_index = 0;
         int right_value = 0;
 
         // Find the highest possible left value and index
@@ -35,16 +34,13 @@ std::string Day3::handlePart1Impl(const std::string& input) {
         // Now, reverse the order and find the right value
         for (int i = line.size() ; i > left_index ; i--) {
 
-            // Get the int equivalent now since c++ doesnt make life easy
             int value = line[i] - '0';
             if(value > right_value) {
                 right_value = value;
-                right_index = i;
             }
         }
 
-        int joltage = std::stoi(std::to_string(left_value) + std::to_string(right_value));
-        total_joltage += joltage;
+        total_joltage += std::stoi(std::to_string(left_value) + std::to_string(right_value));
     }
     return "Total Joltage : " + std::to_string(total_joltage);
 }
@@ -62,19 +58,17 @@ std::string Day3::handlePart2Impl(const std::string& input) {
         // Keep track of the index while we loop through it,
         int start_point = 0;
 
-        while(line.size() > 12) {
+        while(line.size() > 12 && start_point < line.size()) {
 
-            // Set some variables before we loop through the line.
+            // Ensure starting position and gap size,
             int current_index = start_point;
             int current_value = 0;
             int gap_size = line.size() - MAX_SIZE + 1;
 
             // Try to find the highest value given the gap we allow
             for(int i = 0 ; i < gap_size ; i++) {
-                
-                // Get the int equivalent now since c++ doesnt make life easy
-                int value = line[i + start_point] - '0';
 
+                int value = line[i + start_point] - '0';
                 if(value > current_value) {
                     current_value = value;
                     current_index = i + start_point;
@@ -86,16 +80,13 @@ std::string Day3::handlePart2Impl(const std::string& input) {
                 // If the first index of the gap is still the highest value, increment our start index and give it another try,
                 start_point++;
             } else {
-                // If not, we can remove the first entry of our gap, we will eventually hit the same condition applied removing them 1 by 1 if > 1,
+                // If not, we can remove the first entry of our gap,
                 line.erase(start_point, 1);
             }
-
-            // Escape if we exceed and just reduce to max size?
-            if(start_point > line.size()) {
-                line.resize(MAX_SIZE);
-                break;
-            }
         }
+
+        // Resize to ensure we dont exceed line size,
+        line.resize(MAX_SIZE);
 
         total_joltage += std::stoll(line);
     }
